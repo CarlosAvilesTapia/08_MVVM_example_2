a84f6de5041d840c3139da73b55433f7275b4e4e
CAT <cavilest@gmail.com>
2023-08-07 19:53:39 -0400

    Ejercicio individual 08 - Implementanción de dependencias en el gradle. - Permisos de internet y clear text traffic en el manifest.

---
+++ app/src/main/java/cl/cat2814/a08mvvmexample2/MainActivity.kt
@@ -0,0 +1,11 @@
+package cl.cat2814.a08mvvmexample2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
